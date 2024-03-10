0808b55fbbeae7c2506c0d4631a996453a907fcf
Maura <mputzer@smith.edu>
2024-03-10 17:52:48 -0400

    Updated with previous code

--- Building.java
+++ Building.java
@@ -1,9 +1,19 @@
+/**
+ * The building class and its attributes: name, address, nFloors
+ */
 public class Building {
 
     protected String name = "<Name Unknown>";
     protected String address = "<Address Unknown>";
     protected int nFloors = 1;
 
+    
+    /**
+     * The constructor for the building class
+     * @param name
+     * @param address
+     * @param nFloors
+     */
     public Building(String name, String address, int nFloors) {
         if (name != null) { this.name = name; }
         if (address != null) { this.address = address; } 
@@ -13,22 +23,42 @@ public class Building {
         this.nFloors = nFloors;
     }
 
+    /**
+     * Getter for name
+     * @return the name
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Getter for address
+     * @return the address
+     */
     public String getAddress() {
         return this.address;
     }
 
+    /**
+     * Getter for number of floors
+     * @return number of floors
+     */
     public int getFloors() {
         return this.nFloors;
     }
 
+    /**
+     * Takes the building class attributes and combines them into a string
+     * @return string sentence with building class attributes
+     */
     public String toString() {
         return this.name + " is a " + this.nFloors + "-story building located at " + this.address;
     }
 
+    /**
+     * Tester for the building class methods
+     * @param args
+     */
     public static void main(String[] args) {
         Building fordHall = new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4);
         System.out.println(fordHall);
--- reflection.md
+++ reflection.md
@@ -2,3 +2,8 @@ Use this file to record your reflection on this assignment.
 
 - What are your initial impressions of how `Inheritance` affects your programming?
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+
+My initial impressions of how Inheritance affects my program are fairly positive. I can see how inheritance is incredibly useful since it prevents one from continually having to repeat the same code in different classes. It also allows different classes to be connected yet separate in that they aren't necessarily modifying each other (i.e. "child" doesn't modify the "parent" when modifying themself). In terms of what didn't work, I intially was confused by how to use super() in the constructor. I realized that within a constructor for a class, I don't actually need multiple "supers" for each string, int, etc. and can instead just have one. I was also a little confused about how to print the library's catalog in a pretty way because hashtable functionality is still slightly confusing to me.
+
+However, in terms of what worked, I found the hashtable methods like put(), replace(), and remove() to be fairly straightforward and easy to use. When doing this assignment, I also found nesting the method for restock within the sellCoffee method to not actually be as hard as I initally thought it would be. Looking at in-class examples and applying them to this A6 assignment was very helpful and I would tell future students to make sure to review in-class examples before tackling new assignments.
